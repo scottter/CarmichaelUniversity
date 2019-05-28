1533ce9edbaa597e35f73ade3560a262c81b1712
Scott Carmichael <scottacarmichael@gmail.com>
2019-05-28 11:16:43 +1000

    Loading related data on Course and Instructor pages

--- CarmichaelUniversity/App_Start/BundleConfig.cs
+++ CarmichaelUniversity/App_Start/BundleConfig.cs
@@ -23,7 +23,8 @@ namespace CarmichaelUniversity
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.css", 
+                      "~/Content/bootstrap-theme.css",
                       "~/Content/site.css"));
         }
     }
---
+++ CarmichaelUniversity/ViewModels/InstructorIndexData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using CarmichaelUniversity.Models;
+
+namespace CarmichaelUniversity.ViewModels
+{
+    public class InstructorIndexData
+    {
+        public IEnumerable<Instructor> Instructors { get; set; }
+        public IEnumerable<Course> Courses { get; set; }
+        public IEnumerable<Enrollment> Enrollments { get; set; }
+    }
+}
