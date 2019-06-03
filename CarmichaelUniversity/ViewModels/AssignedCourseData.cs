c7cfee9318ae628854615ab7c8408408cd09fbc1
Scott Carmichael <scottacarmichael@gmail.com>
2019-06-03 16:32:29 +1000

    Fixed navbar and form buttons. Updated Course and Instructor functionality and related data

---
+++ CarmichaelUniversity/ViewModels/AssignedCourseData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CarmichaelUniversity.ViewModels
+{
+    public class AssignedCourseData
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
